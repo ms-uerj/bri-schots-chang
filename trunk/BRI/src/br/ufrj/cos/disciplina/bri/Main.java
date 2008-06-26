1297acb141c1b7f6493be8276c0cdd22c11251a7
marceloschots@gmail.com <marceloschots@gmail.com@2a36a3bc-8350-0410-84e1-4bfdcf9201bb>
2008-06-26 06:06:11 +0000

    - Acrescentados alguns comentarios e alguns poucos questionamentos - Alterada a estrutura de ifs pra if-else na classe Main - Alterada da ordem e a estrutura de alguns comandos (vide diff)

--- trunk/BRI/src/br/ufrj/cos/disciplina/bri/Main.java
+++ trunk/BRI/src/br/ufrj/cos/disciplina/bri/Main.java
@@ -21,50 +21,64 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
+		// objetos necessários à leitura do XML
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db;
 		Document myDoc;
+
+		// lista de registros (records) do XML
 		NodeList listRecords;
 
 		try {
 			db = dbf.newDocumentBuilder();
 			myDoc = db.parse(new File("resources/inputs/cf74.xml"));
-			System.out.println(myDoc.getNodeName());
-
+			
+			// inicialização da lista de registros
 			listRecords = myDoc.getElementsByTagName("RECORD");
-
-			//for (int i = 0; i < listRecords.getLength(); i++) {
-				Node nodeRecord = listRecords.item(1);
-
-				NodeList nl = nodeRecord.getChildNodes();
+			
+			// TODO descomentar for (int i = 0; i < listRecords.getLength(); i++) {
+				// instancia o record a ser persistido
 				Record record = new Record();
-				for (int j = 0; j < nl.getLength(); j++) {
+				
+				// captura o record e seu conteúdo
+				Node nodeRecord = listRecords.item(1); // TODO mudar de 1 pra i
+				NodeList recordContents = nodeRecord.getChildNodes();
+				
+				// percorre o conteúdo capturado do record
+				for (int j = 0; j < recordContents.getLength(); j++) {
 					
-					Node n = nl.item(j);
-					if(n.getNodeName().equals("RECORDNUM")){
-						System.out.println(n.getFirstChild().getNodeValue());
-						record.setId(Integer.parseInt(n.getFirstChild().getNodeValue().trim()));
-					}
-					if(n.getNodeName().equals("TITLE")){
-						System.out.println(n.getFirstChild().getNodeValue());
-						record.setTitulo(n.getFirstChild().getNodeValue());
-					}
-					if(n.getNodeName().equals("ABSTRACT")){
-						System.out.println(n.getFirstChild().getNodeValue());
-						record.setResumo(n.getFirstChild().getNodeValue());
-					}
+					// captura o item do record
+					Node recordItem = recordContents.item(j);
 					
-				//}
+					// captura o item conforme seu nome
+					if (recordItem.getFirstChild() != null) {
+						/* TODO todo o conteúdo do XML relativo 
+						 * àquele record específico vem pra cá (sem as tags),
+						 * precisamos ver como fazer (ainda não entendi a captura
+						 * do XML, se será ele inteiro, por registro e tal) */
+					}					
+					if (recordItem.getNodeName().equals("RECORDNUM")){
+						record.setId(Integer.parseInt(recordItem.getFirstChild().getNodeValue().trim()));
+						System.out.println(record.getId()); // TODO remover
+					} else if (recordItem.getNodeName().equals("TITLE")){
+						record.setTitulo(recordItem.getFirstChild().getNodeValue());
+						System.out.println(record.getTitulo()); // TODO remover
+					} else if (recordItem.getNodeName().equals("ABSTRACT")){
+						record.setResumo(recordItem.getFirstChild().getNodeValue());
+						System.out.println(record.getResumo()); // TODO remover
+					}
+				}
 
-			}
+				// persiste o conteúdo capturado do record
+				record.setXml(null); // TODO aqui será colocado o que for capturado acima
+
+			// TODO descomentar }
+				
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
