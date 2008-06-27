62d6298bf731e1970c6589f398c9733b1a01545b
hualin.chang <hualin.chang@2a36a3bc-8350-0410-84e1-4bfdcf9201bb>
2008-06-27 03:48:27 +0000

    now reading all data from Record XML

--- trunk/BRI/src/br/ufrj/cos/disciplina/bri/Main.java
+++ trunk/BRI/src/br/ufrj/cos/disciplina/bri/Main.java
@@ -1,24 +1,13 @@
 package br.ufrj.cos.disciplina.bri;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import br.ufrj.cos.disciplina.bri.model.Record;
 import br.ufrj.cos.disciplina.bri.persistence.JPAResourceBean;
 import br.ufrj.cos.disciplina.bri.persistence.TestConnection;
-
 
 public class Main {
 
@@ -27,69 +16,28 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		// lista de records
+		TestConnection connection = new TestConnection();
+		connection.test();
+
+		// Lista de Records
 		List<Record> listaRecords = new ArrayList<Record>();
 
-		// objetos necessários à leitura do XML
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db;
-		Document myDoc;
-
-		// lista de nós record (dados do XML)
-		NodeList listRecords;
-
-		try {
-			db = dbf.newDocumentBuilder();
-			myDoc = db.parse(new File("resources/inputs/cf74.xml"));
-
-			// inicialização da lista de nós record
-			listRecords = myDoc.getElementsByTagName("RECORD");
-
-			for (int i = 0; i < listRecords.getLength(); i++) {
-
-				// instancia o record a ser persistido
-				Record record = new Record();
-
-				// captura o record e seu conteúdo
-				Node nodeRecord = listRecords.item(i);
-				NodeList recordContents = nodeRecord.getChildNodes();
-
-				// percorre o conteúdo capturado do record
-				for (int j = 0; j < recordContents.getLength(); j++) {
-
-					// captura o item do record
-					Node recordItem = recordContents.item(j);
-
-					// captura o item conforme seu nome
-					if (recordItem.getFirstChild() != null) {
-						/* TODO todo o conteúdo do XML relativo 
-						 * àquele record específico vem pra cá (sem as tags),
-						 * precisamos ver como fazer (ainda não entendi a captura
-						 * do XML, se será ele inteiro, por registro e tal) */
-					}					
-					if (recordItem.getNodeName().equals("RECORDNUM")){
-						record.setId(Integer.parseInt(recordItem.getFirstChild().getNodeValue().trim()));
-					} else if (recordItem.getNodeName().equals("TITLE")){
-						record.setTitulo(recordItem.getFirstChild().getNodeValue());
-					} else if (recordItem.getNodeName().equals("ABSTRACT")){
-						record.setAbztract(recordItem.getFirstChild().getNodeValue());
-					}
-				}
-
-				// persiste o conteúdo capturado do record
-				record.setRecordData(null); // TODO aqui será colocado o que for capturado acima
-				listaRecords.add(record);
-			}
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
+		listaRecords.addAll(Record
+				.parseRecordFromXML("resources/inputs/cf74.xml"));
+		listaRecords.addAll(Record
+				.parseRecordFromXML("resources/inputs/cf75.xml"));
+		listaRecords.addAll(Record
+				.parseRecordFromXML("resources/inputs/cf76.xml"));
+		listaRecords.addAll(Record
+				.parseRecordFromXML("resources/inputs/cf77.xml"));
+		listaRecords.addAll(Record
+				.parseRecordFromXML("resources/inputs/cf78.xml"));
+		listaRecords.addAll(Record
+				.parseRecordFromXML("resources/inputs/cf79.xml"));
 
 		JPAResourceBean jpaResourceBean = new JPAResourceBean();
-		EntityManager em = jpaResourceBean.getEMF("mysql").createEntityManager();
+		EntityManager em = jpaResourceBean.getEMF("mysql")
+				.createEntityManager();
 		try {
 			em.getTransaction().begin();
 
@@ -103,6 +51,7 @@ public class Main {
 		}
 
 		System.out.println("Fim");
+
 	}
 
 }
