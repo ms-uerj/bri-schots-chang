dc2b01e0ff3390247bec17383c128ac1414f2297
hualin.chang <hualin.chang@2a36a3bc-8350-0410-84e1-4bfdcf9201bb>
2008-06-27 20:30:27 +0000

    - Finished! .... almost

--- trunk/BRI/src/br/ufrj/cos/disciplina/bri/LoadData.java
+++ trunk/BRI/src/br/ufrj/cos/disciplina/bri/LoadData.java
@@ -5,19 +5,17 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import br.ufrj.cos.disciplina.bri.algorithm.PrecisionRecall;
 import br.ufrj.cos.disciplina.bri.model.Query;
 import br.ufrj.cos.disciplina.bri.model.Record;
 import br.ufrj.cos.disciplina.bri.persistence.JPAResourceBean;
 import br.ufrj.cos.disciplina.bri.persistence.TestConnection;
 
-public class Main {
+public class LoadData {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
 		TestConnection connection = new TestConnection();
 		connection.test();
 
@@ -40,19 +38,25 @@ public class Main {
 				.parseRecordFromXML("resources/inputs/cf79.xml"));
 		
 		listQueries.addAll(Query.parseQueryFromXML("resources/inputs/cfquery-corrigido.xml"));
-		
-		//for (int i = 0; i < listQueries.size(); i++) {
-			List<Integer> answers;
-			List<Integer> relevants;
+
+		JPAResourceBean jpaResourceBean = new JPAResourceBean();
+		EntityManager em = jpaResourceBean.getEMF("mysql")
+				.createEntityManager();
+		try {
+			em.getTransaction().begin();
+
+			for (int i = 0; i < listRecords.size(); i++) {
+				em.persist(listRecords.get(i));
+			}
 			
-			answers = PrecisionRecall.getQueryElementSet("BOOLEAN", listQueries.get(56), "ABSTRACT");
-			relevants = PrecisionRecall.getRelevantElementSet(listQueries.get(56));
-			
-			PrecisionRecall.getPrecisionRecall(relevants, answers);
-			
-		//}
-		
-		System.out.println("Fim");
+			for (int i = 0; i < listQueries.size(); i++) {
+				em.persist(listQueries.get(i));
+			}
+
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 
 	}
 
---
+++ trunk/BRI/src/br/ufrj/cos/disciplina/bri/model/Point.java
@@ -0,0 +1,23 @@
+package br.ufrj.cos.disciplina.bri.model;
+
+public class Point {
+	private double xRecall;
+	private double yPrecision;
+	
+	public Point() {
+	}
+	
+	public double getXRecall() {
+		return xRecall;
+	}
+	public void setXRecall(double recall) {
+		xRecall = recall;
+	}
+	public double getYPrecision() {
+		return yPrecision;
+	}
+	public void setYPrecision(double precision) {
+		yPrecision = precision;
+	}
+
+}
