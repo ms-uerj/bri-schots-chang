20eb6416a751cd977c88be7eb66c220285596d1a
hualin.chang <hualin.chang@2a36a3bc-8350-0410-84e1-4bfdcf9201bb>
2008-06-27 17:47:46 +0000

    1 - Started implementation of PrecisionRecall evaluation of FULL TEXT SEARCH provided by MYSql

--- trunk/trunk/BRI/src/br/ufrj/cos/disciplina/bri/algorithm/PrecisionRecall.java
+++ trunk/trunk/BRI/src/br/ufrj/cos/disciplina/bri/algorithm/PrecisionRecall.java
@@ -1,11 +1,40 @@
 package br.ufrj.cos.disciplina.bri.algorithm;
 
-public class PrecisionRecall {
-	
-	public static void getPrecisionRecall(String tipoBusca, String campoConsulta) {
-		
-		
-		
-	}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import br.ufrj.cos.disciplina.bri.model.Query;
+
+public class PrecisionRecall {
+
+	public static void getPrecisionRecall(String tipoBusca, Query query, String campoConsulta) {
+		ArrayList<Integer> answers = new ArrayList<Integer>();
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost/bri", "root", "admin");
+			Statement statement = conn.createStatement();
+			ResultSet resultQuestion;
+
+			ResultSet resultAnswers;
+			resultAnswers = statement
+					.executeQuery("SELECT *"
+							+ " FROM record WHERE MATCH (DATA, TITLE, ABSTRACT) AGAINST ('"
+							+ query.getQuestion() + "' IN BOOLEAN MODE)");
+			while (resultAnswers.next()) {
+				answers.add(resultAnswers.getInt("RECORD_ID"));
+			}
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
