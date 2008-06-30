12767517016547cd5f38cd89b00e709fda0fe6a3
hualin.chang <hualin.chang@2a36a3bc-8350-0410-84e1-4bfdcf9201bb>
2008-06-30 14:53:19 +0000

    - added support to 3 types of query (Boolean, Natural Language and Natural Language With Expansion) - added interpolate function

--- trunk/BRI/src/br/ufrj/cos/disciplina/bri/algorithm/PrecisionRecall.java
+++ trunk/BRI/src/br/ufrj/cos/disciplina/bri/algorithm/PrecisionRecall.java
@@ -16,9 +16,9 @@ import br.ufrj.cos.disciplina.bri.model.Point;
 import br.ufrj.cos.disciplina.bri.model.Query;
 
 public class PrecisionRecall {
-	static String TIPO_QUERY_BOOLEAN = "BOOLEAN";
-	static String TIPO_QUERY_NATURAL = "NATURAL";
-	static String TIPO_QUERY_NATURAL_EXT = "NATURAL_EXT";
+	public static String TYPE_QUERY_BOOLEAN = "BOOLEAN";
+	public static String TYPE_QUERY_NATURAL = "NATURAL";
+	public static String TYPE_QUERY_NATURAL_EXT = "NATURAL_EXT";
 
 	/*
 	 * Retorna conjunto de elementos de uma consulta
@@ -32,16 +32,28 @@ public class PrecisionRecall {
 		try {
 
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost/bri", "root", "admin");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/bri", "root", "admin");
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/bri");
 			Statement statement = conn.createStatement();
 			ResultSet resultQuestion;
 
 			ResultSet resultAnswers;
-			resultAnswers = statement.executeQuery("SELECT *"
-					+ " FROM record WHERE MATCH (" + campoConsulta
-					+ ") AGAINST ('" + query.getQuestion()
-					+ "' IN BOOLEAN MODE)");
+			
+			String queryString;
+			
+			queryString = "SELECT *"
+				+ " FROM record WHERE MATCH (" + campoConsulta
+				+ ") AGAINST ('" + query.getQuestion() +"'";
+			if(tipoBusca.equals(PrecisionRecall.TYPE_QUERY_BOOLEAN)){
+				queryString += " IN BOOLEAN MODE)"; 
+			} else if (tipoBusca.equals(PrecisionRecall.TYPE_QUERY_NATURAL)){
+				queryString += ")";
+			} else if (tipoBusca.equals(PrecisionRecall.TYPE_QUERY_NATURAL_EXT)) {
+				queryString += " WITH QUERY EXPANSION)";
+			}
+			
+			resultAnswers = statement.executeQuery(queryString);
+			
 			while (resultAnswers.next()) {
 				answers.add(resultAnswers.getInt("RECORD_ID"));
 			}
@@ -70,7 +82,8 @@ public class PrecisionRecall {
 		return relevants;
 	}
 	
-	public static void getPrecisionRecall(List<Integer> relevants, List<Integer> answers){
+	public static List<Point> getPrecisionRecall(List<Integer> relevants, List<Integer> answers){
+		List<Point> lista = new ArrayList<Point>();
 		
 		double numElementosComum = 0;
 		int sizeRelevants = relevants.size();
@@ -87,25 +100,23 @@ public class PrecisionRecall {
 				numElementosComum++;
 				recall = numElementosComum/sizeRelevants;
 				recalls.add(recall);
-				for (int j = 0; j < answers.size(); j++) {
-						precision = numElementosComum/(j+1);
-						precisions.add(precision);
-				}
 			}
 		}
 		//precision
-		/*numElementosComum = 0;
-		for (int i = 0; i < answers.size(); i++) {
-			if (relevants.contains(answers.get(i))) {
+		numElementosComum = 0;
+		for (int j = 0; j < answers.size(); j++) {
+			if(relevants.contains(answers.get(j))){
 				numElementosComum++;
-				precision = numElementosComum/(i+1);
+				precision = numElementosComum/(j+1);
 				precisions.add(precision);
 			}
-		}*/
-		
-		for (int i = 0; i < recalls.size(); i++) {
-			System.out.println(recalls.get(i)+"\t"+precisions.get(i));
-			
 		}
+		
+		for (int i = 0; i < precisions.size(); i++) {
+			lista.add(new Point(recalls.get(i), precisions.get(i)));
+			System.out.println(recalls.get(i)+"\t"+precisions.get(i));
+		}
+		
+		return lista;
 	}
 }
--- trunk/BRI/src/br/ufrj/cos/disciplina/bri/model/Point.java
+++ trunk/BRI/src/br/ufrj/cos/disciplina/bri/model/Point.java
@@ -1,12 +1,22 @@
 package br.ufrj.cos.disciplina.bri.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Point {
 	private double xRecall;
 	private double yPrecision;
 	
 	public Point() {
+		
 	}
 	
+	public Point(double recall, double precision) {
+		super();
+		xRecall = recall;
+		yPrecision = precision;
+	}
+
 	public double getXRecall() {
 		return xRecall;
 	}
@@ -18,6 +28,37 @@ public class Point {
 	}
 	public void setYPrecision(double precision) {
 		yPrecision = precision;
+	}
+	
+	@Override
+	public String toString() {
+		return xRecall+"\t"+yPrecision;
+	}
+	
+	//Recebe uma  lista de pontos e normaliza em um conjunto de dez pontos.
+	public static List<Point> interpolate(List<Point> lista){
+		List<Point> pontos = new ArrayList<Point>();
+		for (int k = 0; k < 11; k++) {
+			pontos.add(new Point(k, 0.0));
+		}
+		
+		for (int j = 0; j < lista.size(); j++) {
+			Double recall = lista.get(j).getXRecall();
+			Double precision = lista.get(j).getYPrecision();
+			
+			for (int k = 0; k < 11; k++) {
+				if((k < (recall*10)) && ((recall*10) < k+1)){
+					if(precision > pontos.get(k).yPrecision)
+						pontos.get(k).yPrecision = precision; 
+				}
+			}
+		}
+		for (int k = 0; k < 11; k++) {
+			System.out.println(pontos.get(k));
+		}
+		
+		return pontos;
+		
 	}
 
 }
